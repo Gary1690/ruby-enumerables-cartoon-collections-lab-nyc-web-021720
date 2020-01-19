c802d53f4f9dc9773bb93a52ca34ea5ec89d4946
Gary Alberto Cordero Rosa <garycordero1690@gmail.com>
2020-01-19 22:22:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,10 @@ def long_planeteer_calls(planeteer_calls)# code an argument here
   planeteer_calls.any?{|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foods)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.detect{|x| cheese_types.include?(x)}
 end
 
 pp "Holla Mundo".length
